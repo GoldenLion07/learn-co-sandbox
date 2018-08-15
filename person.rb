309647a9a18731f1bdc860acdb25563e48580d3a
Scott M <gmat191@gmail.com>
2018-08-15 21:18:05 +0000

    automatically backed up by learn

---
+++ person.rb
@@ -0,0 +1,26 @@
+class Person
+ 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+ 
+end
+
+kanye = Person.new("Kanye")
+ 
+kanye.name
+
+ 
+kanye.name = "Yeezy"
+kanye.name
